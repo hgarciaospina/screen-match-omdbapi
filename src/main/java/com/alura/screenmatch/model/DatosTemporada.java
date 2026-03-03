30d29a2f2298474cce0295240ebfb68ccab220c7
Henry García <henrygarciaospina@gmail.com>
2026-03-03 15:15:40 -0500

    Modelando las temporadas.

---
+++ src/main/java/com/alura/screenmatch/model/DatosTemporada.java
@@ -0,0 +1,14 @@
+package com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosTemporada (
+        @JsonAlias("Season")
+        Integer temporada,
+        @JsonAlias("Episodes")
+        List<DatosEpisodio> episodios
+){ }
